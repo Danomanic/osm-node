d0dc51b00b9c049943b7947d65c5707322a414b6
Daniel Pomfret <daniel.pomfret@dwp.gsi.gov.uk>
2019-05-14 15:34:58 +0100

    feat(*): convert php example to node

---
+++ index.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const querystring = require('querystring');
+
+var apiid = '';
+var token = 'XXX';
+
+
+var base = 'https://www.onlinescoutmanager.co.uk/';
+var port = 443;
+
+var myEmail = '';
+var myPassword = '';
+
+var userid = 0;
+var secret = '';
+
+const authorise = () => {
+    var parts = [];
+    parts['email'] = myEmail;
+    parts['password'] = myPassword;
+    return perform_query('users.php?action=authorise', parts);
+}
+
+const perform_query = (path, parts) => {
+
+    parts['token'] = token;
+	parts['apiid'] = apiid;
+	
+	if (userid > 0) {
+		parts['userid'] = userid;
+	}
+	if (secret.length == 32) {
+		parts['secret'] = secret;
+    }
+
+    const postData = querystring.stringify(parts);
+    
+    const options = {
+        hostname: base,
+        port: port,
+        path: path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+    
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+    
+    req.on('error', (e) => {
+        console.error(`Problem with request: ${e.message}`);
+    });
+    
+    req.write(postData);
+    req.end();
+}
+
+var json = authorise();
