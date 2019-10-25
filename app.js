ccf580507344491de145d2f9b0f164b9970de1dc
Andrew A. Barber <andrew.barber@racetrackpitstop.co.uk>
2019-10-25 16:53:48 +0100

    added getProgrammeSummary function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const querystring = require('querystring');
 
-const { log } = console;
+const {
+	log
+} = console;
 
 /**
  * Obtain these from OSM Support
@@ -32,51 +34,51 @@ let secret = '';
  * @param {*} parts contains the parts of the query being performed
  */
 async function performQuery(path, parts) {
-  let postParts = [];
-  postParts = parts;
-  postParts.token = token;
-  postParts.apiid = apiid;
+	let postParts = [];
+	postParts = parts;
+	postParts.token = token;
+	postParts.apiid = apiid;
 
-  if (userid > 0) {
-    postParts.userid = userid;
-  }
-  if (secret.length === 32) {
-    postParts.secret = secret;
-  }
+	if (userid > 0) {
+		postParts.userid = userid;
+	}
+	if (secret.length === 32) {
+		postParts.secret = secret;
+	}
 
-  const postData = querystring.stringify(postParts);
+	const postData = querystring.stringify(postParts);
 
-  return new Promise((resolve, reject) => {
-    const url = base + path;
-    axios.post(url, postData).then((response) => {
-      resolve(response.data);
-    })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		const url = base + path;
+		axios.post(url, postData).then((response) => {
+				resolve(response.data);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
 }
 
 /**
  * Authorises the app against the OSM API
  */
 async function authorise() {
-  const parts = [];
-  parts.email = myEmail;
-  parts.password = myPassword;
-  const out = await performQuery('/users.php?action=authorise', parts);
-  userid = out.userid;
-  secret = out.secret;
-  log('Authorised');
+	const parts = [];
+	parts.email = myEmail;
+	parts.password = myPassword;
+	const out = await performQuery('/users.php?action=authorise', parts);
+	userid = out.userid;
+	secret = out.secret;
+	log('Authorised');
 }
 
 /**
  * Get's a list of available Terms
  */
 async function getTerms() {
-  const parts = [];
-  const out = await performQuery('/api.php?action=getTerms', parts);
-  return (out);
+	const parts = [];
+	const out = await performQuery('/api.php?action=getTerms', parts);
+	return (out);
 }
 
 /**
@@ -85,9 +87,9 @@ async function getTerms() {
  * @param {*} termid Term ID
  */
 async function getMembers(sectionid, termid) {
-  const parts = [];
-  const out = await performQuery(`/ext/members/contact/?action=getListOfMembers&sort=dob&sectionid=${sectionid}&termid=${termid}`, parts);
-  return out;
+	const parts = [];
+	const out = await performQuery(`/ext/members/contact/?action=getListOfMembers&sort=dob&sectionid=${sectionid}&termid=${termid}`, parts);
+	return out;
 }
 
 /**
@@ -96,24 +98,38 @@ async function getMembers(sectionid, termid) {
  * @param {*} memberid Member ID
  */
 async function getMemberDetails(sectionid, memberid) {
-  const parts = [];
-  parts.associated_id = memberid;
-  parts.associated_type = 'member';
-  const out = await performQuery(`/ext/customdata/?action=getData&section_id=${sectionid}`, parts);
-  return out;
+	const parts = [];
+	parts.associated_id = memberid;
+	parts.associated_type = 'member';
+	const out = await performQuery(`/ext/customdata/?action=getData&section_id=${sectionid}`, parts);
+	return out;
+}
+
+/**
+ * Get's a summary of Programmes in a given term or all terms
+ * @param {*} sectionid Section ID
+ * @param {*} termid Term ID
+ */
+async function getProgrammeSummary(sectionid, termid) {
+	termid || (termid = '-1');
+	const parts = [];
+	const out = await performQuery(`/ext/programme/?action=getProgrammeSummary&sectionid=${sectionid}&termid=${termid}`, parts);
+	return out;
 }
 
 /**
  * Main
  */
 async function main() {
-  await authorise();
-  /* Get Terms */
-  log(await getTerms());
-  /* Get Members */
-  log(await getMembers());
-  /* Get Member Details */
-  log(await getMemberDetails('<SECTIONID>', '<MEMBERID>'));
+	await authorise();
+	/* Get Terms */
+	log(await getTerms());
+	/* Get Members */
+	log(await getMembers('<SECTIONID>', '<TERMID>'));
+	/* Get Member Details */
+	log(await getMemberDetails('<SECTIONID>', '<MEMBERID>'));
+	/* Get Programme Summary */
+	log(await getProgrammeSummary('<SECTIONID>', '<TERMID(OPTIONAL)'));
 }
 
 main();
