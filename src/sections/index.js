03174004c2bee30cc6e84b5c9dec91331b5b8907
Andrew A. Barber <andrew.barber@racetrackpitstop.co.uk>
2019-11-14 15:10:03 +0000

    added getSectionsNationally

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+const core = require('../core');
+
+/**
+ * Get's a list of all sections registered on OSM nationally
+ */
+
+module.exports.getSectionsNationally = async () => {
+    const parts = [];
+    const out = await core.performQuery(`/ext/members/sectionplanning/?action=listSections`, parts);
+    return out;
+  };
+  
